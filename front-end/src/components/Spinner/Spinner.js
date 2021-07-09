109f6d5cf6ca009e93a70b1c5ce5207713bcc92a
aparg <apargtm@gmail.com>
2021-07-09 23:21:42 +0545

    feat: add spinner

---
+++ front-end/src/components/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SpinnerStyle from './Spinner.module.css'
+
+const Spinner = (props) => {
+    return(
+        <div className = {SpinnerStyle.ldsheart}><div></div></div>
+    )
+}
+
+export default Spinner
