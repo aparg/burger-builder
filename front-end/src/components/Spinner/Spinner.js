8a78dfd56747a89de85b7ba7c345d6b87fc01c65
apar <apargtm@gmail.com>
2023-03-14 13:38:16 +0545

    fix: change spinner and center it

--- front-end/src/components/Spinner/Spinner.js
+++ front-end/src/components/Spinner/Spinner.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import LogoImg from '../../assets/images/burgerLogo.png'
 import SpinnerStyle from './Spinner.module.css'
 
 const Spinner = (props) => {
     return(
-        <div className = {SpinnerStyle.ldsheart}><div></div></div>
+        <div className={SpinnerStyle.spinnerContainer}>
+            <img src={LogoImg} className = {SpinnerStyle.spinner} alt="spinner"></img>
+        </div>
     )
 }
 
