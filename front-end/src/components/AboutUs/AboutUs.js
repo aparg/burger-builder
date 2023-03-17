19c79b3a43b7102acad10dfcc1b7fef1e9194fbe
apar <apargtm@gmail.com>
2023-03-17 23:42:49 +0545

    feat: populate aboutus page

--- front-end/src/components/AboutUs/AboutUs.js
+++ front-end/src/components/AboutUs/AboutUs.js
@@ -5,6 +5,11 @@ const AboutUs = () =>{
     return(
         <div className={AboutUsStyle.main}>
             <h1>About Us</h1>
+            <div className={AboutUsStyle.desc}>
+                <p>This app is designed to break the conventional call and order process for ordering burgers. Now you can get your hands on a delicious Dad's burger without going through any hassle of explaining what you want on the expense of your phone data. 
+                The best part is that you can see what you are ordering and customize your order so that it exactly fits the needs of your taste buds.</p>
+                <p>The order is instantly stored in our database system and the Dad's Burgers team will be working on your order in a matter of seconds.</p>
+            </div>
         </div>
     )
 }
