a75064a4b4b27564e8cc7d261b534c44570937ff
aparg <apargtm@gmail.com>
2023-05-22 23:20:17 +0545

    style: add image to aboutus page

--- front-end/src/components/AboutUs/AboutUs.js
+++ front-end/src/components/AboutUs/AboutUs.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AboutUsStyle from './AboutUs.module.css'
+import Burger from '../../assets/images/PosterBurger/aboutUsBurger.jpeg'
 
 const AboutUs = () =>{
     return(
         <div className={AboutUsStyle.main}>
+            <img alt="burger" className={AboutUsStyle.burger} src={Burger}></img>
             <h1>About Us</h1>
             <div className={AboutUsStyle.desc}>
                 <p>This app is designed to break the conventional call and order process for ordering burgers. Now you can get your hands on a delicious Dad's burger without going through any hassle of explaining what you want on the expense of your phone data. 
