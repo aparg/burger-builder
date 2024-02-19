5614634ea119ec23bb2eb3e0e6c6b391e7f78f0a
aparg <apargtm@gmail.com>
2024-02-19 22:04:10 +0545

    refactor: code for  postgresql

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+// const mysql = require("mysql");
 const bodyParser = require("body-parser");
-require("dotenv").config();
-
-const connection = mysql.createConnection({
+const { Client } = require("pg");
+require("dotenv").config({ path: __dirname + "/.env" });
+const client = new Client({
   host: process.env.HOST,
   user: process.env.DATABASE_NAME,
   password: process.env.DATABASE_PSWD,
@@ -20,61 +20,52 @@ app.get("/", (req, res) => {
   res.send("<h1>API FOR THE BURGER APP<h1>");
 });
 
-app.post("/orders", jsonParser, (req, res) => {
-  connection.query(
-    `INSERT INTO orders VALUES(${Date.now()},${req.body.cheese},${
-      req.body.meat
-    },${req.body.bacon},${req.body.salad});`,
-    (error, response) => {
-      if (error) throw error;
-      res.status(200).end();
-    }
+app.post("/orders", jsonParser, async (req, res) => {
+  await client.query(
+    `INSERT INTO orders (order_id, cheese, meat, bacon, salad) VALUES (${Date.now()},${
+      req.body.cheese
+    },${req.body.meat},${req.body.bacon},${req.body.salad});`
   );
+  res.status(200).end();
 });
 
-app.post("/logincredentials", jsonParser, (req, res) => {
-  connection.query(
-    `SELECT COUNT(username) FROM credentials WHERE username="${req.body.uname}" AND password="${req.body.pswd}"`,
-    (error, response) => {
-      if (response[0]["count(username)"] == 1) res.send("$#*LOGGEDIN*$#");
-      else res.send(false);
-    }
+app.post("/logincredentials", jsonParser, async (req, res) => {
+  const response = await client.query(
+    `SELECT COUNT(username) FROM credentials WHERE username='${req.body.uname}' AND password='${req.body.pswd}'`
   );
+  if (response[0]["count(username)"] == 1) res.send("$#*LOGGEDIN*$#");
+  else res.send(false);
 });
 
 app.get("");
 
-app.get("/ingredients", (req, res) => {
+app.get("/ingredients", async (req, res) => {
   let returnObject = {};
-  connection.query(
-    `SELECT * FROM ingredients ORDER BY ingredient_no;`,
-    (error, response) => {
-      if (error) throw error;
-      response.forEach((el) => {
-        returnObject[el.ingredient] = el.amount;
-      });
-      res.status(200).send(returnObject);
-    }
+  const response = await client.query(
+    `SELECT * FROM ingredients ORDER BY amount;`
   );
+  response.rows.forEach((el) => {
+    returnObject[el.ingredient] = el.amount;
+  });
+  res.status(200).send(returnObject);
 });
 
-app.get("/display", (req, res) => {
+app.get("/display", async (req, res) => {
   let ordersArray = [];
   let newObject = {};
-  connection.query(`SELECT * FROM orders;`, (error, response) => {
-    if (error) throw error;
-    response.forEach((el) => {
-      newObject.order_id = el.order_id;
-      newObject.cheese = el.cheese;
-      newObject.meat = el.meat;
-      newObject.bacon = el.bacon;
-      newObject.salad = el.salad;
-      ordersArray.push({ ...newObject });
-    });
-    res.status(200).send(ordersArray);
+  const response = client.query(`SELECT * FROM orders;`);
+  response.forEach((el) => {
+    newObject.order_id = el.order_id;
+    newObject.cheese = el.cheese;
+    newObject.meat = el.meat;
+    newObject.bacon = el.bacon;
+    newObject.salad = el.salad;
+    ordersArray.push({ ...newObject });
   });
+  res.status(200).send(ordersArray);
 });
 
 app.listen(4000, () => {
   console.log("Listening in port 4000");
+  client.connect();
 });
