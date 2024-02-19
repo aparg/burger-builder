7ec373e85a648a704a3c960f1f7f9899dcd8ab9e
aparg <apargtm@gmail.com>
2024-02-19 17:57:19 +0545

    fix: error catch block

--- server/index.js
+++ server/index.js
@@ -1,73 +1,85 @@
-const express = require('express')
-const app = express()
-const mysql = require('mysql')
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const connection = mysql.createConnection({
-    host:process.env.HOST,
-    user:process.env.DATABASE_NAME,
-    password:process.env.DATABASE_PSWD,
-    database:process.env.DATABASE,
-    port:process.env.PORT || 3306,
-    ssl : {
-        rejectUnauthorized: false
+try {
+  const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PSWD,
+    database: process.env.DATABASE,
+    port: process.env.PORT || 3306,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} catch (err) {
+  console.log("Db not connected");
+  console.log(err);
+}
+let jsonParser = bodyParser.json();
+
+app.get("/", (req, res) => {
+  res.send("<h1>API FOR THE BURGER APP<h1>");
+});
+
+app.post("/orders", jsonParser, (req, res) => {
+  connection.query(
+    `INSERT INTO orders VALUES(${Date.now()},${req.body.cheese},${
+      req.body.meat
+    },${req.body.bacon},${req.body.salad});`,
+    (error, response) => {
+      if (error) throw error;
+      res.status(200).end();
     }
-})
-let jsonParser=bodyParser.json()
+  );
+});
 
-app.get('/',(req,res)=>{
-    res.send('<h1>API FOR THE BURGER APP<h1>')
-})
+app.post("/logincredentials", jsonParser, (req, res) => {
+  connection.query(
+    `SELECT COUNT(username) FROM credentials WHERE username="${req.body.uname}" AND password="${req.body.pswd}"`,
+    (error, response) => {
+      if (response[0]["count(username)"] == 1) res.send("$#*LOGGEDIN*$#");
+      else res.send(false);
+    }
+  );
+});
 
-app.post('/orders',jsonParser,(req,res)=>{
-    connection.query(`INSERT INTO orders VALUES(${Date.now()},${req.body.cheese},${req.body.meat},${req.body.bacon},${req.body.salad});`,
-    (error,response)=>{
-        if(error)throw error 
-        res.status(200).end()
-    })
-  
-})
+app.get("");
 
-app.post('/logincredentials',jsonParser,(req,res)=>{
-    connection.query(`SELECT COUNT(username) FROM credentials WHERE username="${req.body.uname}" AND password="${req.body.pswd}"`,(error,response)=>{
-        if(response[0]["count(username)"]==1)res.send("$#*LOGGEDIN*$#")
-        else
-        res.send(false)
-    })  
-})
+app.get("/ingredients", (req, res) => {
+  let returnObject = {};
+  connection.query(
+    `SELECT * FROM ingredients ORDER BY ingredient_no;`,
+    (error, response) => {
+      if (error) throw error;
+      response.forEach((el) => {
+        returnObject[el.ingredient] = el.amount;
+      });
+      res.status(200).send(returnObject);
+    }
+  );
+});
 
-app.get('')
+app.get("/display", (req, res) => {
+  let ordersArray = [];
+  let newObject = {};
+  connection.query(`SELECT * FROM orders;`, (error, response) => {
+    if (error) throw error;
+    response.forEach((el) => {
+      newObject.order_id = el.order_id;
+      newObject.cheese = el.cheese;
+      newObject.meat = el.meat;
+      newObject.bacon = el.bacon;
+      newObject.salad = el.salad;
+      ordersArray.push({ ...newObject });
+    });
+    res.status(200).send(ordersArray);
+  });
+});
 
-app.get('/ingredients',(req,res)=>{
-    let returnObject={}
-    connection.query(`SELECT * FROM ingredients ORDER BY ingredient_no;`,(error,response)=>{
-        if(error) throw error
-        response.forEach(el=>{
-            returnObject[el.ingredient]=el.amount
-        })
-        res.status(200).send(returnObject)
-    })      
-})
-
-app.get('/display',(req,res)=>{
-    let ordersArray=[]
-    let newObject={}
-    connection.query(`SELECT * FROM orders;`,(error,response)=>{
-        if(error) throw error
-        response.forEach(el=>{
-            newObject.order_id=el.order_id
-            newObject.cheese=el.cheese
-            newObject.meat=el.meat
-            newObject.bacon=el.bacon
-            newObject.salad=el.salad
-            ordersArray.push({...newObject})
-        }
-        )
-        res.status(200).send(ordersArray)        
-    })
-})
-
-app.listen(4000,()=>{
-    console.log("Listening in port 4000")
-})
+app.listen(4000, () => {
+  console.log("Listening in port 4000");
+});
