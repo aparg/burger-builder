7149a750939bd709f62901f4f06d4cf5983ea303
aparg <apargtm@gmail.com>
2024-02-21 01:33:20 +0545

    fix: delete orders from admin

--- front-end/src/components/AdminPage/AdminPage.js
+++ front-end/src/components/AdminPage/AdminPage.js
@@ -1,24 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import AdminLogin from "../AdminLogin/AdminLogin";
 import DisplayOrder from "../DisplayOrder/DisplayOrder";
+import axios from "axios";
 
-function getToken(){
-    const tokenString = sessionStorage.getItem('token')
-    const userToken = JSON.parse(tokenString)
-    return userToken?.token
+function getToken() {
+  const tokenString = sessionStorage.getItem("token");
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token;
 }
 
-function AdminPage(){
-    console.log("load adminpage")
-    const [token,setToken] = useState(getToken())
+function deleteHandler(id) {
+  axios.post("deleteOrder", { id });
+}
+function AdminPage() {
+  console.log("load adminpage");
+  const [token, setToken] = useState(getToken());
 
-    const saveToken = (userToken) => {
-        sessionStorage.setItem('token',JSON.stringify(userToken))
-        setToken(userToken)
-    }
+  const saveToken = (userToken) => {
+    sessionStorage.setItem("token", JSON.stringify(userToken));
+    setToken(userToken);
+  };
 
-    if(token==="$#*LOGGEDIN*$#") return <DisplayOrder/>
-    return <AdminLogin settingToken={(val) => saveToken(val)}/>
+  if (token === "$#*LOGGEDIN*$#")
+    return <DisplayOrder deleteOrder={(id) => deleteHandler(id)} />;
+  return <AdminLogin settingToken={(val) => saveToken(val)} />;
 }
 
-export default AdminPage
+export default AdminPage;
--- front-end/src/components/DisplayOrder/DisplayOrder.js
+++ front-end/src/components/DisplayOrder/DisplayOrder.js
@@ -1,51 +1,59 @@
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import Spinner from "../Spinner/Spinner";
-import axios from '../../axios_orders'
-import DisplayOrderStyle from './DisplayOrder.module.css'
-import DeleteIcon from '../../assets/images/icons/deleteicon.png'
+import axios from "../../axios_orders";
+import DisplayOrderStyle from "./DisplayOrder.module.css";
+import DeleteIcon from "../../assets/images/icons/deleteicon.png";
 
 const DisplayOrder = (props) => {
-    const [ordersObj, setOrders] = useState(null);
+  const [ordersObj, setOrders] = useState(null);
 
-    const getData = () => {
-        console.log("called")
-        axios.get('/display').then((res)=>
-        setOrders({"ordersObj": res.data}))
-    }
+  const getData = () => {
+    console.log("called");
+    axios.post("/display").then((res) => setOrders({ ordersObj: res.data }));
+  };
 
-    //check if order is loaded
-    if(ordersObj){
-        return( <table className={DisplayOrderStyle.displayTable}>
-            <tbody>
-                <tr>
-                    <th>Order Id</th>
-                    <th>Cheese</th>
-                    <th>Meat</th>
-                    <th>Bacon</th>
-                    <th>Salad</th>
-                    <th>Delete</th>
-                </tr>
-                {ordersObj.ordersObj.map(element => {
-                    
-                        return(<tr key={element.order_id}>
-                            <td>{element.order_id}</td>
-                            <td>{element.cheese}</td>
-                            <td>{element.meat}</td>
-                            <td>{element.bacon}</td>
-                            <td>{element.salad}</td>
-                            <td><button onClick={props.deleteHandler}><img className={DisplayOrderStyle.deleteButton} src={DeleteIcon} alt="delete"/></button></td>
-                        </tr>)
-                    
-                })}
-            </tbody>
-        </table>)
-    }
-
-    getData()
-
+  useEffect(() => {
+    getData();
+  }, []);
+  //check if order is loaded
+  if (ordersObj) {
     return (
-       <Spinner/>
-    )
-}
+      <table className={DisplayOrderStyle.displayTable}>
+        <tbody>
+          <tr>
+            <th>Order Id</th>
+            <th>Cheese</th>
+            <th>Meat</th>
+            <th>Bacon</th>
+            <th>Salad</th>
+            <th>Delete</th>
+          </tr>
+          {ordersObj.ordersObj.map((element) => {
+            return (
+              <tr key={element.order_id}>
+                <td>{element.order_id}</td>
+                <td>{element.cheese}</td>
+                <td>{element.meat}</td>
+                <td>{element.bacon}</td>
+                <td>{element.salad}</td>
+                <td
+                  onClick={() => {
+                    props.deleteOrder(element.order_id);
+                    getData();
+                  }}
+                  className={DisplayOrderStyle.delete}
+                >
+                  <img src={DeleteIcon} alt="delete" />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
 
-export default DisplayOrder
+  return <Spinner />;
+};
+
+export default DisplayOrder;
